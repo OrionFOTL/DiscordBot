78d479b5340ab267aa527b7616f903d5528c39b6
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2021-09-14 13:42:50 +0200

    Basic bot

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace DiscordBot
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -19,6 +15,12 @@ namespace DiscordBot
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
+                    services.ConfigureBotServices();
                 });
+
+        private static void ConfigureBotServices(this IServiceCollection services)
+        {
+
+        }
     }
 }
--- DiscordBot/Worker.cs
+++ DiscordBot/Worker.cs
@@ -1,8 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,19 +11,38 @@ namespace DiscordBot
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly string _discordBotToken;
+        private DiscordSocketClient _client;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _discordBotToken = configuration["DiscordBotToken"];
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        public override async Task StartAsync(CancellationToken cancellationToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            _logger.LogInformation("Starting discord bot");
+
+            _client = new DiscordSocketClient();
+            _client.Log += Client_Log;
+
+            await _client.LoginAsync(TokenType.Bot, _discordBotToken);
+            await _client.StartAsync();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken) => await Task.CompletedTask;
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping bot");
+            await _client.LogoutAsync();
+        }
+
+        private async Task Client_Log(LogMessage arg)
+        {
+            _logger.LogInformation(arg.ToString());
+            await Task.CompletedTask;
         }
     }
 }
