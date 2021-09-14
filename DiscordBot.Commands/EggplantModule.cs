3fb46938c1cdec0db8d007026cb0072c4494c5a1
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2021-09-14 19:35:06 +0200

    Basic command module

---
+++ DiscordBot.Commands/EggplantModule.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.Commands
+{
+    public class EggplantModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("dog")]
+        public async Task PrintEggplant()
+        {
+            await Context.Channel.SendMessageAsync("🌭");
+        }
+
+        [Command("orion")]
+        public async Task ReactToOrion()
+        {
+            await Context.Message.AddReactionAsync(new Emoji("🌌"));
+            await Context.Message.AddReactionAsync(new Emoji("💫"));
+        }
+
+        [Command("thicc")]
+        public async Task ThiccenThis([Remainder] string textToThiccen)
+        {
+            var embed = new EmbedBuilder
+            {
+                Author = new EmbedAuthorBuilder().WithName("me"),
+                Title = "Interesting",
+            }.Build();
+
+            await Context.Message.ReplyAsync($"**{textToThiccen}**", embed: embed);
+        }
+
+        [Command("user")]
+        public async Task ShowUser(IUser user)
+        {
+            await ReplyAsync(user.CreatedAt.ToString());
+        }
+    }
+}
