9778bc8976d007fdfffcbd308971c128a828fe97
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2021-09-15 13:08:57 +0200

    Logging failed commands

--- DiscordBot/MessageHandler.cs
+++ DiscordBot/MessageHandler.cs
@@ -4,6 +4,7 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using DiscordBot.Commands;
+using Microsoft.Extensions.Logging;
 
 namespace DiscordBot
 {
@@ -11,12 +12,14 @@ namespace DiscordBot
     {
         private const char _commandPrefix = '$';
 
+        private readonly ILogger<MessageHandler> _logger;
         private readonly DiscordSocketClient _client;
         private readonly CommandService _commands;
         private readonly IServiceProvider _serviceProvider;
 
-        public MessageHandler(DiscordSocketClient client, CommandService commands, IServiceProvider serviceProvider)
+        public MessageHandler(ILogger<MessageHandler> logger, DiscordSocketClient client, CommandService commands, IServiceProvider serviceProvider)
         {
+            _logger = logger;
             _client = client;
             _commands = commands;
             _serviceProvider = serviceProvider;
@@ -24,9 +27,20 @@ namespace DiscordBot
 
         public async Task InstallCommandsAsync()
         {
+            _commands.CommandExecuted += CommandExecuted;
             _client.MessageReceived += HandleMessageAsync;
 
             await _commands.AddModulesAsync(typeof(EggplantModule).Assembly, _serviceProvider);
+        }
+
+        private async Task CommandExecuted(Optional<CommandInfo> commandInfo, ICommandContext commandContext, IResult result)
+        {
+            if (!result.IsSuccess)
+            {
+                _logger.LogError(result.ErrorReason);
+            }
+
+            await Task.CompletedTask;
         }
 
         private async Task HandleMessageAsync(SocketMessage message)
