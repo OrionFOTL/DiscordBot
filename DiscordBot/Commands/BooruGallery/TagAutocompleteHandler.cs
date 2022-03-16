ba4a39ab94765000420a48a379b8ea8c3f3338d0
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2022-03-16 20:28:22 +0100

    File-scoped namespace

--- DiscordBot/Commands/BooruGallery/TagAutocompleteHandler.cs
+++ DiscordBot/Commands/BooruGallery/TagAutocompleteHandler.cs
@@ -4,82 +4,81 @@ using Discord.Interactions;
 using DiscordBot.Model;
 using DiscordBot.Services.Interface;
 
-namespace DiscordBot.Commands.BooruGallery
+namespace DiscordBot.Commands.BooruGallery;
+
+public class TagAutocompleteHandler : AutocompleteHandler
 {
-    public class TagAutocompleteHandler : AutocompleteHandler
+    private readonly ITagClient _tagClient;
+    private readonly ILogger<TagAutocompleteHandler> _logger;
+
+    private static readonly ConcurrentDictionary<ulong, CancellationTokenSource> _userAutocompleteTokens = new();
+
+    public TagAutocompleteHandler(ITagClient tagClient, ILogger<TagAutocompleteHandler> logger)
     {
-        private readonly ITagClient _tagClient;
-        private readonly ILogger<TagAutocompleteHandler> _logger;
+        _tagClient = tagClient;
+        _logger = logger;
+    }
 
-        private static readonly ConcurrentDictionary<ulong, CancellationTokenSource> _userAutocompleteTokens = new();
+    public override async Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
+    {
+        var tag = autocompleteInteraction.Data.Current.Value as string;
 
-        public TagAutocompleteHandler(ITagClient tagClient, ILogger<TagAutocompleteHandler> logger)
+        if (string.IsNullOrWhiteSpace(tag) || tag.Length <= 1)
         {
-            _tagClient = tagClient;
-            _logger = logger;
+            return AutocompletionResult.FromSuccess();
         }
 
-        public override async Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
+        tag = tag.Replace(' ', '_');
+        IEnumerable<Tag> matchingTags = await GetSimilarTags(tag, context.User.Id);
+
+        if (!matchingTags.Any())
         {
-            var tag = autocompleteInteraction.Data.Current.Value as string;
-
-            if (string.IsNullOrWhiteSpace(tag) || tag.Length <= 1)
-            {
-                return AutocompletionResult.FromSuccess();
-            }
-
-            tag = tag.Replace(' ', '_');
-            IEnumerable<Tag> matchingTags = await GetSimilarTags(tag, context.User.Id);
-
-            if (!matchingTags.Any())
-            {
-                return AutocompletionResult.FromSuccess();
-            }
-
-            var autocompleteResults = matchingTags.Select(t => new AutocompleteResult()
-            {
-                Name = $"{t.PrettyName} ({t.Count})",
-                Value = t.CodedName
-            });
-
-            return AutocompletionResult.FromSuccess(autocompleteResults);
+            return AutocompletionResult.FromSuccess();
         }
 
-        private async Task<IEnumerable<Tag>> GetSimilarTags(string tag, ulong uid)
+        var autocompleteResults = matchingTags.Select(t => new AutocompleteResult()
         {
-            if (_userAutocompleteTokens.TryGetValue(uid, out CancellationTokenSource tokenSource))
-            {
-                tokenSource.Cancel();
-            }
+            Name = $"{t.PrettyName} ({t.Count})",
+            Value = t.CodedName
+        });
 
-            var cancellationTokenSource = new CancellationTokenSource();
-            _userAutocompleteTokens[uid] = cancellationTokenSource;
+        return AutocompletionResult.FromSuccess(autocompleteResults);
+    }
 
-            var cancellationToken = cancellationTokenSource.Token;
-
-            try
-            {
-                await Task.Delay(100, cancellationToken);
-            }
-            catch (TaskCanceledException)
-            {
-                _logger.LogError(DateTimeOffset.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt") + " Sending argument: " + tag + " was cancelled by exception");
-                return Array.Empty<Tag>();
-            }
-
-            if (cancellationToken.IsCancellationRequested)
-            {
-                _logger.LogError(DateTimeOffset.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt") + " Sending argument: " + tag + " was cancelled by if");
-                return Array.Empty<Tag>();
-            }
-
-            _logger.LogWarning("Getting suggestions for tag: {tag}", tag);
-
-            var tags = await _tagClient.GetSimilarTags(tag);
-
-            _logger.LogWarning("Got suggestion: {suggestion} for tag: {tag}", tags.FirstOrDefault().CodedName, tag);
-
-            return tags;
+    private async Task<IEnumerable<Tag>> GetSimilarTags(string tag, ulong uid)
+    {
+        if (_userAutocompleteTokens.TryGetValue(uid, out CancellationTokenSource tokenSource))
+        {
+            tokenSource.Cancel();
         }
+
+        var cancellationTokenSource = new CancellationTokenSource();
+        _userAutocompleteTokens[uid] = cancellationTokenSource;
+
+        var cancellationToken = cancellationTokenSource.Token;
+
+        try
+        {
+            await Task.Delay(100, cancellationToken);
+        }
+        catch (TaskCanceledException)
+        {
+            _logger.LogError(DateTimeOffset.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt") + " Sending argument: " + tag + " was cancelled by exception");
+            return Array.Empty<Tag>();
+        }
+
+        if (cancellationToken.IsCancellationRequested)
+        {
+            _logger.LogError(DateTimeOffset.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt") + " Sending argument: " + tag + " was cancelled by if");
+            return Array.Empty<Tag>();
+        }
+
+        _logger.LogWarning("Getting suggestions for tag: {tag}", tag);
+
+        var tags = await _tagClient.GetSimilarTags(tag);
+
+        _logger.LogWarning("Got suggestion: {suggestion} for tag: {tag}", tags.FirstOrDefault().CodedName, tag);
+
+        return tags;
     }
 }
