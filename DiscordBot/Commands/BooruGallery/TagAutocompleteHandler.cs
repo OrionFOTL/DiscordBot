0b574fc3ea6a02faa63fca304c60a6ed1ebd61ff
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2022-03-22 19:07:20 +0100

    Removed cancelling autocomplete requests

--- DiscordBot/Commands/BooruGallery/TagAutocompleteHandler.cs
+++ DiscordBot/Commands/BooruGallery/TagAutocompleteHandler.cs
@@ -11,8 +11,6 @@ public class TagAutocompleteHandler : AutocompleteHandler
     private readonly ITagClient _tagClient;
     private readonly ILogger<TagAutocompleteHandler> _logger;
 
-    private static readonly ConcurrentDictionary<ulong, CancellationTokenSource> _userAutocompleteTokens = new();
-
     public TagAutocompleteHandler(ITagClient tagClient, ILogger<TagAutocompleteHandler> logger)
     {
         _tagClient = tagClient;
@@ -47,32 +45,6 @@ public class TagAutocompleteHandler : AutocompleteHandler
 
     private async Task<IEnumerable<Tag>> GetSimilarTags(string tag, ulong uid)
     {
-        if (_userAutocompleteTokens.TryGetValue(uid, out CancellationTokenSource tokenSource))
-        {
-            tokenSource.Cancel();
-        }
-
-        var cancellationTokenSource = new CancellationTokenSource();
-        _userAutocompleteTokens[uid] = cancellationTokenSource;
-
-        var cancellationToken = cancellationTokenSource.Token;
-
-        try
-        {
-            await Task.Delay(100, cancellationToken);
-        }
-        catch (TaskCanceledException)
-        {
-            _logger.LogError(DateTimeOffset.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt") + " Sending argument: " + tag + " was cancelled by exception");
-            return Array.Empty<Tag>();
-        }
-
-        if (cancellationToken.IsCancellationRequested)
-        {
-            _logger.LogError(DateTimeOffset.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt") + " Sending argument: " + tag + " was cancelled by if");
-            return Array.Empty<Tag>();
-        }
-
         _logger.LogWarning("Getting suggestions for tag: {tag}", tag);
 
         var tags = await _tagClient.GetSimilarTags(tag);
